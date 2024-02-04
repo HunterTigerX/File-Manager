f1b70841d1c1d58cefa26c97c5d89ae1eb5c7244
HunterTigerX <HunterTigerX@gmail.com>
2024-02-04 21:39:59 +0300

    feat: add compress and decompress operations, refactored code

--- src/start.js
+++ src/start.js
@@ -7,6 +7,7 @@ import { createReadStream } from "fs";
 import { createWriteStream } from "fs";
 import { EOL } from "os";
 import crypto from "crypto";
+import zlib from "zlib";
 
 const usernameUnredacted = process.argv.slice(2).toString();
 const startDirectory = os.homedir();
@@ -38,24 +39,27 @@ if (usernameUnredacted.includes("--username=")) {
 }
 
 function showCommands() {
-  console.log("Here is a list of commands that you can use")
-  console.log("up")
-  console.log("cd path_to_directory")
-  console.log("ls")
-  console.log("cat path_to_file")
-  console.log("add new_file_name")
-  console.log("rn path_to_file new_filename")
-  console.log("cp path_to_file path_to_new_directory")
-  console.log("mv path_to_file path_to_new_directory")
-  console.log("rm path_to_file")
-  console.log("os --EOL")
-  console.log("os --cpus")
-  console.log("os --homedir")
-  console.log("os --username")
-  console.log("os --architecture")
-  console.log("hash path_to_file")
-  console.log("compress path_to_file path_to_destination")
-  console.log("decompress path_to_file path_to_destination")
+  console.log("Here is a list of commands that you can use");
+  console.log("up");
+  console.log("cd path_to_directory");
+  console.log("ls");
+  console.log("cat path_to_file");
+  console.log("add new_file_name");
+  console.log("rn path_to_file new_filename");
+  console.log("cp path_to_file path_to_new_directory");
+  console.log("mv path_to_file path_to_new_directory");
+  console.log("rm path_to_file");
+  console.log("os --EOL");
+  console.log("os --cpus");
+  console.log("os --homedir");
+  console.log("os --username");
+  console.log("os --architecture");
+  console.log("hash path_to_file");
+  console.log("compress path_to_file path_to_destination");
+  console.log("decompress path_to_file path_to_destination");
+  console.log(
+    'default compress extention is ".br" if full path was not specified'
+  );
 }
 
 function goUp() {
@@ -66,37 +70,40 @@ function goodByeMessage() {
   //   console.log(`Thank you for using File Manager, ${username}, goodbye!`);
 }
 
-function missing(err, funcName) {
-  if (err.code === "ENOENT" && funcName === "copyMove") {
+function showError(err, todo) {
+  if (err.code === "ENOENT" && todo === "copyMove") {
     console.log(
-      `There are no such directory to copy to or you used a path to a file instead of a path to directory`
+      `There is no such directory to copy to or you used a path to a file instead of a path to directory`
     );
-  } else if (err.code === "ENOENT") {
+  } else if (err.code === "ENOENT" && todo !== 'ignore') {
     console.log(`File or directory is missing`);
   } else {
     console.log(err);
   }
 }
 
-async function readFile(pathToFile) {
-  const fileStream = createReadStream(pathToFile);
-  return new Promise((resolve, reject) => {
-    fileStream.on("data", (data) => {
-      process.stdout.write(data);
-    });
 
-    fileStream.on("end", () => {
-      process.stdout.write("\n");
-      resolve();
-    });
-
-    fileStream.on("error", (error) => {
-      reject(error);
-    });
-  });
-}
 
 async function checkExistence(filteredPath, functionName) {
+  
+  async function readFile(pathToFile) {
+    const fileStream = createReadStream(pathToFile);
+    return new Promise((resolve, reject) => {
+      fileStream.on("data", (data) => {
+        process.stdout.write(data);
+      });
+  
+      fileStream.on("end", () => {
+        process.stdout.write("\n");
+        resolve();
+      });
+  
+      fileStream.on("error", (error) => {
+        reject(error);
+      });
+    });
+  }
+
   try {
     const testedPath = await fs.stat(filteredPath);
 
@@ -114,12 +121,12 @@ async function checkExistence(filteredPath, functionName) {
       }
     }
   } catch (err) {
-    missing(err);
+    showError(err);
   }
 }
 
 async function createNewPath(pathToCheck) {
-  if (path.isAbsolute(pathToCheck)) {
+  if (path.isAbsolute(pathToCheck) && pathToCheck !== pathSeparator) {
     return pathToCheck;
   } else {
     return join(currentDirectory, pathToCheck);
@@ -155,7 +162,7 @@ async function listDirectoriesAndFiles() {
 
     console.table(results);
   } catch (err) {
-    missing(err);
+    showError(err);
   }
 }
 
@@ -165,7 +172,7 @@ async function createNewFile(newFileName) {
       await fs.writeFile(newFileName, "");
       console.log(`New file ${newFileName} was created`);
     } catch (err) {
-      missing(err);
+      showError(err);
     }
   }
   try {
@@ -197,7 +204,7 @@ async function renameFile(oldFilePath, newName) {
           `You can't have a file without extention and a folder with the same name at the same time`
         );
       } else {
-        missing(err);
+        showError(err);
       }
     }
   }
@@ -217,14 +224,14 @@ async function renameFile(oldFilePath, newName) {
       } catch (err) {
         await rename(dirName, fileName);
         if (err.code !== "ENOENT") {
-          missing(err, fileName);
+          showError(err, fileName);
         }
       }
     } else {
       console.log(`You are trying to rename a folder, not a file`);
     }
   } catch (err) {
-    missing(err);
+    showError(err);
   }
 }
 
@@ -253,7 +260,7 @@ async function copyMoveFile(oldFile, newLocation, command) {
                   try {
                     await fs.unlink(oldFile);
                   } catch (err) {
-                    missing(err);
+                    showError(err);
                   }
                 }
                 resolve();
@@ -268,13 +275,13 @@ async function copyMoveFile(oldFile, newLocation, command) {
           console.log(`${newLocation} is not a directory`);
         }
       } catch (err) {
-        missing(err, "copyMove");
+        showError(err, "copyMove");
       }
     } else {
       console.log(`${oldFile} is a directory, not a file`);
     }
   } catch (err) {
-    missing(err);
+    showError(err);
   }
 }
 
@@ -287,54 +294,143 @@ async function removeFile(pathToAfile) {
         await fs.unlink(pathToAfile);
         console.log(`file ${fileName} was successfully removed`);
       } catch (err) {
-        missing(err);
+        showError(err);
       }
     } else if (newLocationChecked.isDirectory()) {
       console.log(`You are trying to remove a directory`);
     }
   } catch (err) {
-    missing(err);
+    showError(err);
   }
 }
 
 async function calcHash(filePath) {
   console.log(filePath);
-    try {
-        await fs.access(filePath);
-        const content = await fs.readFile(filePath, "utf-8");
-        const hash = crypto.createHash("sha256").update(content).digest("hex");
-        console.log(hash);
-    } catch (err) {
-        missing(err);
-    }
+  try {
+    await fs.access(filePath);
+    const content = await fs.readFile(filePath, "utf-8");
+    const hash = crypto.createHash("sha256").update(content).digest("hex");
+    console.log(hash);
+  } catch (err) {
+    showError(err);
+  }
 }
 
 async function systemInfo(argument) {
-  if (argument === '--EOL') {
-    if (EOL === '\r\n') {
-      console.log('Your default system End-Of-Line is \\r\\n')
-    } else if (EOL === '\r') {
-      console.log('Your default system End-Of-Line is \\r')
-    } else if (EOL === '\n') {
-      console.log('Your default system End-Of-Line is \\n')
-  } 
-  } else if (argument === '--cpus') {
+  if (argument === "--EOL") {
+    if (EOL === "\r\n") {
+      console.log("Your default system End-Of-Line is \\r\\n");
+    } else if (EOL === "\r") {
+      console.log("Your default system End-Of-Line is \\r");
+    } else if (EOL === "\n") {
+      console.log("Your default system End-Of-Line is \\n");
+    }
+  } else if (argument === "--cpus") {
     const cpuCores = os.cpus().length;
-    console.log(`Total number of CPU's cores - ${cpuCores}`)
+    console.log(`Total number of CPU's cores - ${cpuCores}`);
     os.cpus().map((core, index) => {
-        console.log(`Core ${index + 1} name is ${core.model} and it's clock rate is ${Math.round((core.speed * 0.10))/100}GHz`)
-    })
-  }
-   else if (argument === '--homedir') {
-    console.log(`Your home directory is ${startDirectory}`)
-  }
-   else if (argument === '--username') {
-    console.log(`Your username is ${os.userInfo().username} and your machine name is ${os.hostname}`)
-  }
-   else if (argument === '--architecture') {
-    console.log(`Your CPU architecture is ${process.arch}`)
+      console.log(
+        `Core ${index + 1} name is ${core.model} and it's clock rate is ${
+          Math.round(core.speed * 0.1) / 100
+        }GHz`
+      );
+    });
+  } else if (argument === "--homedir") {
+    console.log(`Your home directory is ${startDirectory}`);
+  } else if (argument === "--username") {
+    console.log(
+      `Your username is ${os.userInfo().username} and your machine name is ${
+        os.hostname
+      }`
+    );
+  } else if (argument === "--architecture") {
+    console.log(`Your CPU architecture is ${process.arch}`);
   } else {
-    console.log('wrong argument, type helf for the list of supported commands')
+    console.log("wrong argument, type helf for the list of supported commands");
+  }
+}
+
+async function compressionHub(pathToFile, pathToFolder) {
+  function compress(output) {
+    const streamInput = createReadStream(pathToFile);
+    const streamOutput = createWriteStream(output);
+    const gzip = zlib.createBrotliCompress();
+    streamInput.pipe(gzip).pipe(streamOutput);
+    console.log(`file was successfully compressed`)
+  }
+
+  try {
+    const checkedFile = await fs.stat(pathToFile);
+    const firstFileNameOnly = path.parse(pathToFile).name;
+
+    if (checkedFile.isFile()) {
+      try {
+        const checkedFolder = await fs.stat(pathToFolder);
+        // something that was used as a second argument already exists
+        if (checkedFolder.isDirectory()) {
+          // User used path as a second argument and we are autogenerating name
+          pathToFolder = join(pathToFolder, `${firstFileNameOnly}.br`);
+          compress(pathToFolder);
+        } else if (checkedFolder.isFile()) {
+          compress(pathToFolder);
+        }
+      } catch (err) {
+        // No file or directory was found that was used as a second argument
+        if (err !== "ENOENT") {
+          compress(pathToFolder);
+        } else {
+          showError(err);
+        }
+      }
+    } else {
+      console.log(
+        "Your are using path to directory as a first argument, not a path to a file"
+      );
+    }
+  } catch (err) {
+    showError(err);
+  }
+}
+
+async function decompressionHub(pathToFile, pathToFolder) {
+  function decompress(output) {
+    const streamInput = createReadStream(pathToFile);
+    const streamOutput = createWriteStream(output);
+    const gunzip = zlib.createBrotliDecompress();
+    streamInput.pipe(gunzip).pipe(streamOutput);
+    console.log(`file was successfully decompressed`)
+  }
+
+  try {
+    const checkedFile = await fs.stat(pathToFile);
+    const firstFileNameOnly = path.parse(pathToFile).name;
+
+    if (checkedFile.isFile()) {
+      try {
+        const checkedFolder = await fs.stat(pathToFolder);
+        // something that was used as a second argument already exists
+        if (checkedFolder.isDirectory()) {
+          // User used path as a second argument and we are autogenerating name
+          pathToFolder = join(pathToFolder, `${firstFileNameOnly}.txt`);
+          decompress(pathToFolder);
+        } else if (checkedFolder.isFile()) {
+          decompress(pathToFolder);
+        }
+      } catch (err) {
+        // No file or directory was found that was used as a second argument
+        if (err !== "ENOENT") {
+          decompress(pathToFolder);
+        } else {
+          showError(err);
+        }
+      }
+    } else {
+      console.log(
+        "Your are using path to directory as a first argument, not a path to a file"
+      );
+    }
+  } catch (err) {
+    showError(err);
   }
 }
 
@@ -376,7 +472,7 @@ function userInteraction() {
           } else {
             goUp();
           }
-        }else if (command === "help") {
+        } else if (command === "help") {
           showCommands();
         }
       } else if (oneArgumentCommands.includes(command)) {
@@ -390,7 +486,7 @@ function userInteraction() {
           } else if (command === "rm") {
             await removeFile(await createNewPath(argument1));
           } else if (command === "os") {
-            await systemInfo((argument1));
+            await systemInfo(argument1);
           } else if (command === "hash") {
             await calcHash(await createNewPath(argument1));
           }
@@ -400,6 +496,7 @@ function userInteraction() {
           console.log("Second argument is missing");
         } else {
           if (command === "rn") {
+            // peredelat
             if (argument2.includes(pathSeparator)) {
               console.log(
                 "Second argument should be a new file name, not a path with a file name"
@@ -412,6 +509,16 @@ function userInteraction() {
               await createNewPath(argument1),
               await createNewPath(argument2),
               command
+            );
+          } else if (command === "compress") {
+            await compressionHub(
+              await createNewPath(argument1),
+              await createNewPath(argument2)
+            );
+          } else if (command === "decompress") {
+            await decompressionHub(
+              await createNewPath(argument1),
+              await createNewPath(argument2)
             );
           }
         }
